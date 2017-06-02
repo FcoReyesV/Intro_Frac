f78812b57c5a7aca452d33d0e26116bdf17bdcc1
FcoReyesV <lmethod1234@gmail.com>
2017-06-02 18:21:33 -0500

    Botones de agregar y quitar fracciones

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	
-	crearObjetosDraggables(1);
+	crearObjetosDraggables(8);
 	var contador_contenedores=1;
 	var $flecha_agregar_contenedor = $('#flecha-agregar-contenedor');
 	var $flecha_quitar_contenedor = $('#flecha-quitar-contenedor');
@@ -13,9 +13,40 @@ $(document).ready(function(){
 	var $numerador_texto = $('#numerador-texto');
 	var $denominador_texto = $('#denominador-texto');
 
+	var $numerador_agregar_btn = $('#numerador-agregar-btn');
+	var $numerador_quitar_btn = $('#numerador-quitar-btn');
+
+	
+
 	$flecha_agregar_contenedor.click(function(event) {
 		contador_contenedores++;
 		$num_max .text(contador_contenedores);
+		destruirContenedorFigura(contador_contenedores);
+		crearContenedorFigura(contador_contenedores);
+
+
+
+		if(contador_numerador<(contador_denominador*contador_contenedores)){
+			if(contador_numerador==0){
+
+				$numerador_quitar_btn.attr('disabled','true');
+				$numerador_quitar_btn.removeClass('add-flecha-btn');
+				$numerador_quitar_btn.addClass('add-flecha-btn-desact');
+			}else{
+				$numerador_agregar_btn.removeClass('add-flecha-btn-desact');
+				$numerador_agregar_btn.addClass('add-flecha-btn');
+				$numerador_quitar_btn.removeAttr('disabled','true');
+				$numerador_agregar_btn.removeAttr('disabled','true');
+			}
+			
+		}else{
+			$numerador_agregar_btn.attr('disabled','true');
+			$numerador_agregar_btn.removeClass('add-flecha-btn');
+			$numerador_agregar_btn.addClass('add-flecha-btn-desact');
+		}
+
+			
+
 		if(contador_contenedores==6){
 			$flecha_agregar_contenedor.attr('disabled','true');
 			$flecha_agregar_contenedor.removeClass('add-flecha-btn');
@@ -28,12 +59,26 @@ $(document).ready(function(){
 			$flecha_quitar_contenedor.addClass('add-flecha-btn');
 			
 		}
+
 		//crearContenedorFigura(contador_contenedores);
 	});
 
 	$flecha_quitar_contenedor.click(function(event) {
+		destruirContenedorFigura(contador_contenedores);
 		contador_contenedores--;
 		$num_max .text(contador_contenedores);
+		crearContenedorFigura(contador_contenedores);
+		if(contador_numerador>(contador_contenedores*contador_denominador)){
+			contador_numerador=contador_contenedores*contador_denominador;
+			$numerador_texto.text(contador_numerador);
+		}
+		if(contador_numerador==(contador_contenedores*contador_denominador)){
+			$numerador_agregar_btn.attr('disabled','true');
+			$numerador_agregar_btn.removeClass('add-flecha-btn');
+			$numerador_agregar_btn.addClass('add-flecha-btn-desact');
+		}
+		
+
 		if(contador_contenedores==1){
 			$flecha_quitar_contenedor.attr('disabled','true');
 			$flecha_agregar_contenedor.removeAttr('disabled','true');
@@ -50,26 +95,49 @@ $(document).ready(function(){
 	});
 
 	$denominador_agregar_btn.click(function(event) {
+		destruirFiguraDND(contador_denominador);
 		contador_denominador++;
 		$denominador_texto .text(contador_denominador);
+		creaFiguraDND(contador_denominador);
 		if(contador_denominador==8){
 			$denominador_agregar_btn.attr('disabled','true');
 			$denominador_agregar_btn.removeClass('add-flecha-btn');
 			$denominador_agregar_btn.addClass('add-flecha-btn-desact');
 
 		}else{
+			
 			$denominador_agregar_btn.removeAttr('disabled','true');
 			$denominador_quitar_btn.removeAttr('disabled','true');
 			$denominador_quitar_btn.removeClass('add-flecha-btn-desact');
 			$denominador_quitar_btn.addClass('add-flecha-btn');
 			
+
+			$numerador_agregar_btn.removeClass('add-flecha-btn-desact');
+			$numerador_agregar_btn.addClass('add-flecha-btn');
+			//$numerador_quitar_btn.removeAttr('disabled','true');
+			$numerador_agregar_btn.removeAttr('disabled','true');
 		}
 		//crearContenedorFigura(contador_contenedores);
 	});
 
 	$denominador_quitar_btn.click(function(event) {
+		destruirFiguraDND(contador_denominador);
 		contador_denominador--;
+		creaFiguraDND(contador_denominador);
 		$denominador_texto .text(contador_denominador);
+		if(contador_denominador==contador_numerador){
+					$numerador_agregar_btn.attr('disabled','true');
+					$numerador_agregar_btn.removeClass('add-flecha-btn');
+					$numerador_agregar_btn.addClass('add-flecha-btn-desact');
+		}
+		if(contador_denominador<contador_numerador){			
+				contador_numerador--;
+				$numerador_texto.text(contador_numerador);	
+		}
+		if(contador_numerador>(contador_contenedores*contador_denominador)){
+			contador_numerador=contador_contenedores*contador_denominador;
+			$numerador_texto.text(contador_numerador);
+		}
 		if(contador_denominador==1){
 			$denominador_quitar_btn.attr('disabled','true');
 			$denominador_agregar_btn.removeAttr('disabled','true');
@@ -80,30 +148,90 @@ $(document).ready(function(){
 			$denominador_agregar_btn.addClass('add-flecha-btn');
 			$denominador_quitar_btn.removeAttr('disabled','true');
 			$denominador_agregar_btn.removeAttr('disabled','true');
+				
+
+		//crearContenedorFigura(contador_contenedores);
+		}
+	});
+
+	$numerador_agregar_btn.click(function(event) {
+		contador_numerador++;
+		 $numerador_texto.text(contador_numerador);
+		if(contador_denominador*contador_contenedores==contador_numerador){
+			$numerador_agregar_btn.attr('disabled','true');
+			$numerador_agregar_btn.removeClass('add-flecha-btn');
+			$numerador_agregar_btn.addClass('add-flecha-btn-desact');
+			$numerador_quitar_btn.removeAttr('disabled','true');
+			$numerador_quitar_btn.removeClass('add-flecha-btn-desact');
+			$numerador_quitar_btn.addClass('add-flecha-btn');
+
+		}else{
+			$numerador_agregar_btn.removeAttr('disabled','true');
+			$numerador_quitar_btn.removeAttr('disabled','true');
+			$numerador_quitar_btn.removeClass('add-flecha-btn-desact');
+			$numerador_quitar_btn.addClass('add-flecha-btn');
 			
 		}
 		//crearContenedorFigura(contador_contenedores);
 	});
 
+	$numerador_quitar_btn.click(function(event) {
+		contador_numerador--;
+		$numerador_texto.text(contador_numerador);
+		if(contador_numerador==0){
+			$numerador_quitar_btn.attr('disabled','true');
+			$numerador_agregar_btn.removeAttr('disabled','true');
+			$numerador_quitar_btn.removeClass('add-flecha-btn');
+			$numerador_quitar_btn.addClass('add-flecha-btn-desact');
+			$numerador_agregar_btn.removeClass('add-flecha-btn-desact');
+			$numerador_agregar_btn.addClass('add-flecha-btn');
+		}else{
+			$numerador_agregar_btn.removeClass('add-flecha-btn-desact');
+			$numerador_agregar_btn.addClass('add-flecha-btn');
+			$numerador_quitar_btn.removeAttr('disabled','true');
+			$numerador_agregar_btn.removeAttr('disabled','true');
+			
+		}
+		//crearContenedorFigura(contador_contenedores);
+	});
 
 	
 });
 
 function crearObjetosDraggables(num_figura){
 	for ( var i=0; i<3; i++ ) {
-	    $('<div class="figuras-color figura-rectangulo-horizontal-'+num_figura+'"></div>')
+	    $('<div class="figura-propiedades figuras-color figura-rectangulo-horizontal-'+num_figura+'"></div>')
 	    	.attr( 'id', 'figura'+i )
 	    	.appendTo( '#contenedor-objetos-draggables' ).draggable( {
 		      containment: '.bloque-central',
 		      stack: '#contenedor-objetos-draggables div',
 		      cursor: 'move',
 		      revert: true
-		    });
+		});
+	    $('#figura'+i).css({
+	    	top: '10px',
+	    	right: 10*i+'px'
+	    });
 	}
 }
 function crearContenedorFigura(num_contenedores){
 	for(var i=0; i<num_contenedores;i++)
 		$('<div class="contenedor-figura"><div>').attr('id','contenedorFigura'+i).appendTo('.contenedor-principal');
 }
+function destruirContenedorFigura(num_contenedores){
+	for(var i=0; i<num_contenedores;i++)
+		$('#contenedorFigura'+i).remove();
+}
 
+function creaFiguraDND(contador_denominador){
+	for(var i=0;i<contador_denominador;i++){
+		$('<div class="figura-rectangulo-horizontal-'+contador_denominador+'"> ').attr('id','figuraDND'+i+'CF0').appendTo('#contenedorFigura0');
+	}
+}
+
+function destruirFiguraDND(contador_denominador){
+	for(var i=0;i<contador_denominador;i++){
+		$('#figuraDND'+i+'CF0').remove();
+	}
+}
 
